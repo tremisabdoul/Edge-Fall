c13a5acf14a0900fad64b47dfe03b46d9f0fbd64
tremisabdoul <tremisabdoul@gmail.com>
2023-11-07 18:19:41 +0100

    SHEEESH les mouvements et la cam

--- CharacterBody3D.gd
+++ CharacterBody3D.gd
@@ -1,28 +1,35 @@
 extends CharacterBody3D
 
 
-var speed = 5.0
-var jump_velocity = 7
+var speed = 5.
+var wallride_stamina = 1.
+var jump_velocity = 7.
 var sensibility: Vector2 = -Vector2(.01, .01)
-var friction = 2
+var friction = 2.
 var is_wallriding = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") * 4
 
+func _enter_tree():
+	get_window().mode = Window.MODE_FULLSCREEN
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotation.y += event.relative.x * sensibility.x
 		$Head.rotation.x = clamp(-PI/2, $Head.rotation.x + event.relative.y * sensibility.y, PI/2)
-		$Head.rotation.z += event.relative.x * sensibility.x / 10
-		$Head.fov = min((120 + 1/friction + $Head.fov + abs(event.relative.x * sensibility.x * 5))/2, 160)
+		$Head.rotation.z += event.relative.x * sensibility.x / 10 * (Vector2(velocity.x, velocity.z).length() / 10)
+		#$Head.fov = min((120 + 1/friction + $Head.fov + abs(event.relative.x * sensibility.x * 5))/2, 160)
 
 func _physics_process(delta):
-	gravity = ProjectSettings.get_setting("physics/3d/default_gravity") * 4
-	jump_velocity = 7*2
+	var current_speed = Vector2(velocity.x, velocity.z).length()
 	var input = Input.get_vector("left", "right", "forward", "backward").normalized()
 	var tickspeed = speed
 	var was_wallriding = is_wallriding
+	$Head.fov = ($Head.fov * 7 + 90 + (89*current_speed)/(current_speed+20))/8
+	gravity = ProjectSettings.get_setting("physics/3d/default_gravity") * 4
+	jump_velocity = 7*2
 	is_wallriding = (
 		!is_on_floor()
 		and
@@ -30,26 +37,44 @@ func _physics_process(delta):
 		and
 		($WallRide/Top/Back.is_colliding() or $WallRide/Top/Left.is_colliding() or $WallRide/Top/Right.is_colliding())
 		and
-		!Input.is_action_pressed("ui_accept")
+		(!Input.is_action_pressed("jump") or !Input.is_action_just_pressed("jump"))
+		and
+		(wallride_stamina > 0)
 	)
-	#!Input.is_action_pressed("ui_accept")
+	if wallride_stamina < 0:
+		is_wallriding = false
+		wallride_stamina = 0
+
+	if is_wallriding:
+		wallride_stamina -= delta
+	#!Input.is_action_pressed("jump")
 	if was_wallriding and !is_wallriding and !is_on_floor():
 		var direction = $Head.global_transform.basis.z * (input.y + 5*(-float($WallRide/Top/Back.is_colliding())))
 		direction += $Head.global_transform.basis.x * (input.x + 5*(float($WallRide/Top/Left.is_colliding())-float($WallRide/Top/Right.is_colliding())))
 		velocity.y = 7
 		velocity += direction * tickspeed
+		wallride_stamina -= .2
+	elif is_on_floor():
+		wallride_stamina = (wallride_stamina + 1) / (delta*60*2)
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	$Head/WallRideStaminaBar.set_size(Vector2(128 * wallride_stamina, 4))
+	$Head/SpeedBar.set_size(Vector2(current_speed*4, 4))
+	$Head/SpeedIndicator.set_text(str(round(current_speed*100)/100))
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
 		tickspeed *= 2
-
 	if is_wallriding:
-		if Input.is_action_pressed("ui_accept"):
-			velocity += $Head.global_transform.basis.x * (float($WallRide/Top/Left.is_colliding())-float($WallRide/Top/Right.is_colliding())) * tickspeed
+		#if Input.is_action_pressed("jump"):
+		#q 	velocity += $Head.global_transform.basis.x * (float($WallRide/Top/Left.is_colliding())-float($WallRide/Top/Right.is_colliding())) * tickspeed
 		var direction = $Head.global_transform.basis.z * input.y
 		direction += $Head.global_transform.basis.x * input.x
-		friction += 1.2 * .1
-		friction /= 1.1
+		direction.y *= current_speed/10
+		clamp(direction.y, -6, 3)
+		#friction += 1.2 * .1
+		#friction /= 1.1
+		#friction += 1.0000001 * .01
+		#friction = min(friction / 1.01, 1.25)
 		velocity.y = clamp(velocity.y, -6, 3)
 		velocity.y -= gravity * delta
 		velocity += direction * tickspeed / 2
@@ -58,9 +83,9 @@ func _physics_process(delta):
 		velocity.z /= friction
 		var wallriding_dir = float($WallRide/Top/Left.is_colliding())-float($WallRide/Top/Right.is_colliding())
 		$Head.rotation.z -= velocity.dot(transform.basis.x) / 1000
-		$Head.rotation.z -= wallriding_dir * .15
+		$Head.rotation.z -= wallriding_dir * .2
 		$Head.rotation.z /= 1.1
-		$Head.rotation.z += wallriding_dir * .15
+		$Head.rotation.z += wallriding_dir * .2
 	elif is_on_floor():
 		var direction = transform.basis * Vector3(input.x, 0, input.y)
 		velocity += direction * tickspeed
@@ -72,8 +97,8 @@ func _physics_process(delta):
 		$Head.rotation.z /= 1.1
 	else:
 		var direction = transform.basis * Vector3(input.x, 0, input.y)
-		velocity += direction * tickspeed / 2
-		friction += 1.001 * .01
+		velocity += direction * tickspeed /3
+		friction += 1.0000001 * .01
 		friction = min(friction / 1.01, 1.25)
 		velocity.y -= gravity * delta
 		velocity.x /= friction
@@ -81,7 +106,7 @@ func _physics_process(delta):
 		$Head.rotation.z -= velocity.dot(transform.basis.x) / 1000
 		$Head.rotation.z /= 1.1
 
-	print(friction)
-	print(int(velocity.length()))
+	#print(friction)
+	#print(int(velocity.length()))
 
 	move_and_slide()
